0987c410306e8fe259928ef9b173c4e6fa0a202c
AceChiu <Acelove0930>
2024-07-12 20:12:48 -0700

    feat: spring 3.3.1, JDK 17,

---
+++ spring3.0/src/main/java/com/ace/AceApplication.java
@@ -0,0 +1,24 @@
+package com.ace;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Slf4j
+@EntityScan(basePackages = { "com.ace" })
+@EnableJpaRepositories(basePackages = { "com.ace" })
+@SpringBootApplication(scanBasePackages = { "com.ace" })
+public class AceApplication {
+
+	public static void main(String[] args) {
+		try {
+			SpringApplication.run(AceApplication.class, args);
+			System.out.println("Server startup done");
+		} catch (Exception e) {
+			log.error("server support error", e);
+		}
+	}
+
+}
